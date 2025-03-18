c72b5a70f6efa67be144895a6f28cb25ccd5f259
Devendra Mishra <devendra@mavonic.com>
2025-03-18 19:27:14 +0530

    initial: initial commit added 2 doc files and some configuration changes

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,14 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'placehold.co'
+      }
+    ],
+  },
 };
 
 export default withMDX(config);
